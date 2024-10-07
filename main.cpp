48269d28bb3caf5a38bd222fa462696f8f91e261
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2024-10-07 12:08:58 +0900

    コールバック関数の実装途中

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <windows.h>
+#include <time.h>
+
+typedef void (*pFunc)(int *);
+
+//コールバック関数
+void Result(int *dice)
+{
+	if(dice / 2 == 0)
+	{
+		printf("偶数です\n");
+	}
+	else
+	{
+		printf("奇数です\n");
+	}
+}
+
+void SetTimeout(pFunc p,int second)
+{
+	//コールバック関数を呼び出す
+	Sleep(second*1000);
+
+	srand(static_cast<unsigned int>(time(0)));
+	int dice = rand() % 6;
+
+
+	p(&dice);
+}
 
 int main()
 {
-
+	
+	
 	return 0;
 }
