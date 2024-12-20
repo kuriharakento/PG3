d670fd4f88bc050f372ac117b1096dceae0bb0f5
kento <k023g0033@g.neec.ac.jp>
2024-12-20 11:07:34 +0900

    OK

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
-#include <stdio.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
-int main()
-{
+std::mutex mtx;
+std::condition_variable cv;
+int current_thread = 1;
 
-	return 0;
+void print_thread(int id) {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [id]() { return id == current_thread; }); // 指定された順番まで待つ
+    std::cout << "thread " << id << std::endl;
+    ++current_thread;
+    cv.notify_all(); // 次のスレッドを実行可能にする
+}
+
+int main() {
+    // スレッドを作成
+    std::thread t1(print_thread, 1);
+    std::thread t2(print_thread, 2);
+    std::thread t3(print_thread, 3);
+
+    // すべてのスレッドが終了するのを待機
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
 }
